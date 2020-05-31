9aa5a55bd65e2f5be4e2f32db7f61030cfa906a1
gatesyp <steven@deephire.com>
2020-05-31 09:29:03 -0400

    initial commit

---
+++ src/pages/form/basic-form/index.jsx
@@ -0,0 +1,77 @@
+import { InfoCircleOutlined } from '@ant-design/icons';
+import {
+  Typography,
+  Button,
+  Card,
+  DatePicker,
+  Input,
+  Form,
+  InputNumber,
+  Radio,
+  Select,
+  Tooltip,
+  Row,
+  Col,
+} from 'antd';
+import axios from 'axios';
+
+import { connect, FormattedMessage, formatMessage } from 'umi';
+import React, { useState, useEffect } from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import styles from './style.less';
+
+const { Title } = Typography;
+
+const FormItem = Form.Item;
+const { Option } = Select;
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+
+const BasicForm = (props) => {
+  const [callsMade, setCallsMade] = useState(0);
+
+  const runReport = async () => {
+    axios
+      .get(
+        'www.google.com',
+        // 'https://hn.algolia.com/api/v1/search?query=redux',
+        // 'https://api.outreach.io/api/v2/calls?filter[updatedAt]=2020-05-29..inf',
+      )
+      .then((result) => {
+        console.log(result);
+      });
+    //   setCallsMade(result.data);
+
+    setCallsMade(callsMade + 1);
+  };
+
+  return (
+    <PageHeaderWrapper content="Check in on your progress each day to your goal. ">
+      <Button type="primary" onClick={runReport}>
+        Refresh!!!
+      </Button>
+
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card title="Calls" bordered={false}>
+            Calls Made: <Title>{callsMade}</Title>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Appointments Set" bordered={false}>
+            TEST
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Dispositions" bordered={false}>
+            Card content
+          </Card>
+        </Col>
+      </Row>
+    </PageHeaderWrapper>
+  );
+};
+
+export default connect(({ loading }) => ({
+  submitting: loading.effects['formAndbasicForm/submitRegularForm'],
+}))(BasicForm);
