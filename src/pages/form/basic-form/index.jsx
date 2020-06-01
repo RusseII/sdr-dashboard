fd0e22e86eeedfe46a73488ad09fab24d174d69a
gatesyp <steven@deephire.com>
2020-05-31 21:23:08 -0400

    added some react hook errors im running into

--- src/pages/form/basic-form/index.jsx
+++ src/pages/form/basic-form/index.jsx
@@ -1,10 +1,4 @@
-import {
-  Button,
-  Card,
-  Row,
-  Col,
-  Statistic
-} from 'antd';
+import { Button, Card, Row, Col, Statistic, Select } from 'antd';
 
 import { connect } from 'umi';
 import React, { useState, useEffect } from 'react';
@@ -12,31 +6,90 @@ import { PageHeaderWrapper } from '@ant-design/pro-layout';
 
 import { getOutreachCalls } from '@/services/api';
 
+const { Option } = Select;
 
 const BasicForm = () => {
   const [callsMade, setCallsMade] = useState(0);
+  const [selectedUser, setSelectedUser] = useState(2);
+  const [teamData, setTeamData] = useState();
+  const updateDashboard = (userId) => {
+    console.log(teamData);
+    // const myData = teamData.data.filter((val) => val.relationships.user.data === userId);
+    /* let displayedData = [];
+	let newData = teamData.data.relationships.data.user.data.id;
+	teamData.data.forEach(function(newData){
+		if(newData === userId){
+			displayedData.push()
+		}
+	};
+	setCallsMade(teamData.meta.count);
+	
+
+	}
+	setCallsMade(teamData.data) */
+  };
+
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+    setSelectedUser(value);
+    updateDashboard(value);
+  }
 
   const runReport = async () => {
-    const outreachCalls = await getOutreachCalls()
-    const data = outreachCalls
-    // do things with the outreach data here 
-    // outreachCallsD
+    const initial_url = `https://api.outreach.io/api/v2/calls?filter[updatedAt]=2020-05-29..inf`;
+    // const initial_url = `https://api.outreach.io/api/v2/calls?filter%5BupdatedAt%5D=2020-05-29..inf&page%5Boffset%5D=300`;
+    const outreachCalls = await getOutreachCalls(initial_url);
 
+    const data = outreachCalls;
+    console.log(data);
+
+    // start paginating
+    let url = data.links.next;
+    let pageData = '';
+    console.log('checking');
+    if (url) {
+      do {
+        pageData = await getOutreachCalls(url);
+        // data.data.push(pageData.data);
+        // data.data.concat(pageData.data);
+        data.data = [...data.data, ...pageData.data];
+        url = pageData.links.next;
+        console.log(pageData);
+      } while (pageData.links.next);
+    }
+    // console.log(data);
+    useEffect(() => {
+      console.log(teamData);
+    }, [teamData]);
+    setTeamData(data);
+
+    // updateDashboard(2);
+
+    // do things with the outreach data here
+    // outreachCallsD
     //   setCallsMade(result.data);
-    setCallsMade(callsMade + 1);
+
+    // setCallsMade(data.meta.count);
   };
 
   // this runs the first time the page loads and runs the report
   useEffect(() => {
-    runReport()
-  }, [])
+    runReport();
+  }, []);
 
   return (
-    <PageHeaderWrapper extraContent={<Button type="primary" onClick={runReport}>
-      Refresh!!!
-  </Button>} content="Check in on your progress each day to your goal. ">
-
-
+    <PageHeaderWrapper
+      extraContent={
+        <Button type="primary" onClick={runReport}>
+          Refresh!!!
+        </Button>
+      }
+      content="Check in on your progress each day to your goal. "
+    >
+      <Select defaultValue="Cameron" style={{ width: 120 }} onChange={handleChange}>
+        <Option value="2">Cameron</Option>
+        <Option value="3">Mikey</Option>
+      </Select>
       <Row gutter={16}>
         <Col span={8}>
           <Card bordered={false}>
--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,15 @@
 import request from '@/utils/request';
 
-
 // add cors-anywhere to beginging of URL to get around CROS origin error
 // cors-anywhere runs server side, and just mimics the api call you send it
 // then returns back to data
-const outreachUrl = 'https://cors-anywhere.herokuapp.com/https://api.outreach.io/api/v2/'
-const token = 'Bearer example_token_haha!'
+const outreachUrl = 'https://cors-anywhere.herokuapp.com/';
+const token =
+  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdGV2ZW5AZGVlcGhpcmUuY29tIiwiaWF0IjoxNTkwOTcxNjY1LCJleHAiOjE1OTA5Nzg4NjUsImJlbnRvIjoiYXBwMmIiLCJvcmdfdXNlcl9pZCI6MSwiYXVkIjoiRGVlcEhpcmUiLCJzY29wZXMiOiJBQkFBUUE9PSJ9.XbqvVN_vbChiIjaFCGNuGNB7tWoGdnog_QxhaTFwyQE';
 
 const headers = {
-    authorization: token,
-    'Content-Type': 'application/vnd.api+json',
-}
+  authorization: token,
+  'Content-Type': 'application/vnd.api+json',
+};
 
-
-export const getOutreachCalls = async () => request(`${outreachUrl}calls?filter[updatedAt]=2020-05-29..inf`, { headers })
-
-
+export const getOutreachCalls = async (url) => request(`${outreachUrl}${url}`, { headers });
