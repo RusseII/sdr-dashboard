13641d573070352d0e138766a36c9991319c5300
russell <russell@deephire.com>
2020-07-18 12:25:50 -0500

    simplify routing

--- config/config.js
+++ config/config.js
@@ -38,6 +38,7 @@ export default defineConfig({
       path: '/',
       component: '../layouts/BlankLayout',
       routes: [
+        { path: '/', redirect: '/dashboard' },
         {
           path: '/user',
           component: '../layouts/UserLayout',
@@ -74,13 +75,14 @@ export default defineConfig({
           component: '../layouts/BasicLayout',
           authority: ['admin', 'user'],
           routes: [
+            { path: '/', redirect: '/dashboard' },
             {
-                    name: 'Dashboard',
-                    icon: 'smile',
-                    path: '/dashboard',
-                    component: './dashboard/',
-                   },
-          
+              name: 'Dashboard',
+              icon: 'smile',
+              path: '/dashboard',
+              component: './dashboard/',
+            },
+
             {
               component: '404',
             },
@@ -120,8 +122,8 @@ export default defineConfig({
           const antdProPath = match[1].replace('.less', '');
           const arr = winPath(antdProPath)
             .split('/')
-            .map(a => a.replace(/([A-Z])/g, '-$1'))
-            .map(a => a.toLowerCase());
+            .map((a) => a.replace(/([A-Z])/g, '-$1'))
+            .map((a) => a.toLowerCase());
           return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-');
         }
 
